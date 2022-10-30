67f3b5fccc943539b37d9867f00b13549473ed21
Tanishq Bhatt <tanishq.bhatt10@gmail.com>
2022-10-30 21:59:42 +0530

    added news page

--- js/news.js
+++ js/news.js
@@ -10,6 +10,9 @@ async function Load(){
     
     //Write Below//
 
+    LoadNwCard(json["newscard"])
+
+    LoadNwCont(json["newscont"])
 
     LoadFooter(json["footer"])
 
@@ -117,6 +120,228 @@ function LoadNav(obj){
     let head = document.createElement("header");
     head.appendChild(nav);
     document.body.appendChild(head);
+}
+
+
+function LoadNwCard(obj){
+    console.log(obj)
+    let div1 = document.createElement("div");
+    div1.classList.add("container");
+    let div2 = document.createElement("div");
+    div2.style.backgroundImage = obj.bgimg_1;
+    div2.classList.add("panel"); 
+    let a1 = document.createElement("a");
+    a1.href = obj.herf1_1;
+    a1.classList.add("color-me");
+    let h31 = document.createElement("h3");
+    let a2 = document.createElement("a");
+    a2.innerText = obj.txt_1;
+    a2.href = obj.herf1_2;
+    a2.classList.add("color-me");
+    
+    let div3 = document.createElement("div");
+    div3.style.backgroundImage = obj.bgimg_2;
+    div3.classList.add("panel");
+    let a3 = document.createElement("a");
+    a3.href = obj.href2_1;
+    a3.classList.add("color-me");
+    let h32 = document.createElement("h3");
+    let a4 = document.createElement("a");
+    a4.href = obj.href2_2;
+    a4.innerText = obj.txt_2;
+    a4.classList.add("color-me");
+
+    let div4 = document.createElement("div");
+    div4.style.backgroundImage = obj.bgimg_3;
+    div4.classList.add("panel");
+    let a5 = document.createElement("a");
+    a5.href = obj.href3_1;
+    a5.classList.add("color-me");
+    let h33 = document.createElement("h3");
+    let a6 = document.createElement("a");
+    a6.href = obj.href3_2;
+    a6.innerText = obj.txt_3;
+    a6.classList.add("color-me");
+
+    let div = document.createElement("div");
+    div.style.backgroundImage = obj.bgimg_4;
+    div.classList.add("panel");
+    let a7 = document.createElement("a");
+    a7.href = obj.href4_1;
+    a7.classList.add("color-me");
+    let h3 = document.createElement("h3");
+    let a = document.createElement("a");
+    a.href = obj.href4_2;
+    a.innerText = obj.txt_4;
+    a.classList.add("color-me");
+    div.appendChild(a);
+    
+    div.appendChild(h3);
+    div1.appendChild(div2)
+    div2.appendChild(a1)
+    div2.appendChild(h31)
+    h31.appendChild(a2)
+    div1.appendChild(div3)
+    div3.appendChild(a3)
+    div3.appendChild(h32)
+    h32.appendChild(a4)
+    div1.appendChild(div4)
+    div4.appendChild(a5)
+    div4.appendChild(h33)
+    h33.appendChild(a6)
+    div1.appendChild(div)
+    div.appendChild(a7)
+    div.appendChild(h3)
+    h3.appendChild(a)
+    
+    document.body.appendChild(div1);
+    
+
+
+}
+
+function LoadNwCont(obj){
+    console.log(obj)
+    let div5 = document.createElement("div");
+    div5.classList.add("news-cont");
+    let h1 = document.createElement("h1");
+    h1.innerText = obj.txt_h1;
+    div5.appendChild(h1)
+    let h3 = document.createElement("h3");
+    h3.innerText = obj.txt_h3;
+    div5.appendChild(h3)
+    
+    let ul = document.createElement("ul");
+    let li1 = document.createElement("li");
+    li1.classList.add("newslist");
+    li1.style.border = "1px solid white";
+    let img1 = document.createElement("img");
+    img1.src = obj.img_link1;
+    li1.appendChild(img1);
+    let div6 = document.createElement("div");
+    div6.classList.add("news-section");
+    let a7 = document.createElement("a");
+    let h21 = document.createElement("h2");
+    h21.innerText = obj.li_1;
+    div6.appendChild(a7);
+    a7.appendChild(h21);
+    li1.appendChild(div6);
+    let p1 = document.createElement("p");
+    p1.innerText = obj.p_1;
+    let a8 = document.createElement("a");
+    a8.style.color = "blue";
+    a8.innerText = obj.p_2;
+    p1.appendChild(a8);
+    div6.appendChild(p1); 
+    
+    let li2 = document.createElement("li");
+    li2.classList.add("newslist");
+    li2.style.border = "1px solid black";
+    let div7 = document.createElement("div");
+    div7.classList.add("news-section");
+    let a9 = document.createElement("a");
+    let h22 = document.createElement("h2");
+    h22.innerText = obj.li_2;
+    div7.appendChild(a9);
+    a9.appendChild(h22);
+    li2.appendChild(div7);
+    let p2 = document.createElement("p");
+    p2.innerText = obj.p_3;
+    let a0 = document.createElement("a");
+    a0.style.color = "blue";
+    a0.innerText = obj.p_4;
+    li2.appendChild(a0)
+    p2.appendChild(a0);
+    div7.appendChild(p2); 
+    let img2 = document.createElement("img");
+    img2.src = obj.img_link2;
+    li2.appendChild(img2);
+    
+    let li3  = document.createElement("li");
+    li3.classList.add("newslist");
+    li3.style.border = "1px solid white";
+    let img3 = document.createElement("img");
+    img3.src = obj.img_link3;
+    li3.appendChild(img3);
+    let div8 = document.createElement("div");
+    div8.classList.add("news-section");
+    let a11 = document.createElement("a");
+    let h25 = document.createElement("h2");
+    h25.innerText = obj.li_3;
+    a11.appendChild(h25);
+    li3.appendChild(div8);
+    div8.appendChild(h25)
+    let p3 = document.createElement("p");
+    p3.innerText = obj.p_5;
+    let a12 = document.createElement("a");
+    a12.style.color = "blue";
+    a12.innerText = obj.p_6;
+    p3.appendChild(a12);
+    div8.appendChild(p3); 
+    
+    let li4 = document.createElement("li");
+    li4.classList.add("newslist");
+    li4.style.border = "1px solid black";
+    let div0 = document.createElement("div");
+    div0.classList.add("news-section");
+    let a13 = document.createElement("a");
+    a13.href = obj.a1;
+    let h23 = document.createElement("h2");
+    h23.innerText = obj.li_4;
+    div0.appendChild(a13);
+    a13.appendChild(h23);
+    li4.appendChild(div0);
+    let p4 = document.createElement("p");
+    p4.innerText = obj.p_7;
+    let a15 = document.createElement("a");
+    a13.href = obj.a1;
+    a15.style.color = "blue";
+    a15.innerText = obj.p_8;
+    li4.appendChild(a15)
+    p4.appendChild(a15);
+    div0.appendChild(p4); 
+    let img4 = document.createElement("img");
+    img4.src = obj.img_link4;
+    li4.appendChild(img4);
+
+    let li = document.createElement("li");
+    li.classList.add("newslist");
+    li.style.border = "1px solid white";
+    let img = document.createElement("img");
+    img.src = obj.img_link5;
+    li.appendChild(img);
+    let div = document.createElement("div");
+    div.classList.add("news-section");
+    let a16 = document.createElement("a");
+    let h2 = document.createElement("h2");
+    h2.innerText = obj.li_5;
+    div.appendChild(a16);
+    a16.appendChild(h2);
+    li.appendChild(div);
+    let p = document.createElement("p");
+    p.innerText = obj.p_9;
+    let a = document.createElement("a");
+    a.style.color = "blue";
+    a.innerText = obj.p_0;
+    p.appendChild(a);
+    div.appendChild(p);
+
+    document.body.appendChild(div5);
+    div5.appendChild(li1)
+    div5.appendChild(li2)
+    div5.appendChild(li3)
+    div5.appendChild(li4)
+    div5.appendChild(li)
+
+   
+    
+
+    let btn = document.createElement("Button");
+    btn.style.margin = "8vh auto";
+    btn.classList.add("more", "btn41-43" , "btn-42");
+    btn.innerText = "Load More";
+    document.body.appendChild(btn);
+
 }
 
 
